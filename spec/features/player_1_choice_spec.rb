896b1d56ca152715c30c8cfb98a8b882543423b7
MarcinBalejko <mbbm9@o2.pl>
2019-08-01 00:56:17 +0200

    :white_check_mark: Player 2 can select one of the characters

---
+++ spec/features/player_1_choice_spec.rb
@@ -0,0 +1,8 @@
+feature 'Player 2 character select' do
+    scenario 'Player 2 can select one of the characters' do
+        sign_in_and_play
+        click_button('Peter')
+        click_button('Chicken')
+        expect(page).not_to have_content('Error')
+    end
+end
