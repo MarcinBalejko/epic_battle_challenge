abe0282f981727b478d45da80fcab6665c83e177
MarcinBalejko <mbbm9@o2.pl>
2019-07-31 23:46:28 +0200

    :white_check_mark: Game class retrieves 2 players

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,14 @@
 require_relative 'player'
 class Game
-    attr_reader :players
-    def initialize(player_1)
-        @players = player_1 
+    attr_reader :current_turn
+    def initialize(player_1, player_2)
+        @players = [player_1, player_2]
+        @current_turn = player_1
+    end
+    def player_1
+        @players.first
+    end
+    def player_2
+        @players.last
     end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,17 @@
 require 'game'
 describe Game do 
-    subject(:game) { described_class.new(player_1) }
+    subject(:game) { described_class.new(player_1, player_2) }
     let(:player_1) { Player.new }
+    let(:player_2) { Player.new }
     it 'retrieves Player 1' do
-        expect(subject.players).to eq(player_1)
+        expect(subject.player_1).to eq(player_1)
+    end
+end
+describe Game do 
+    subject(:game) { described_class.new(player_1, player_2) }
+    let(:player_1) { Player.new }
+    let(:player_2) { Player.new }
+    it 'retrieves Player 2' do
+        expect(subject.player_2).to eq(player_2)
     end
 end
