9a95f2d5e8dd3c67d6ec71d0ace91969f4b4714e
MarcinBalejko <mbbm9@o2.pl>
2019-07-31 23:56:43 +0200

    :recycle: Refactored

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,15 +3,16 @@ describe Game do
     subject(:game) { described_class.new(player_1, player_2) }
     let(:player_1) { Player.new }
     let(:player_2) { Player.new }
-    it 'retrieves Player 1' do
-        expect(subject.player_1).to eq(player_1)
+
+    describe '#player_1' do
+        it 'retrieves Player 1' do
+            expect(subject.player_1).to eq(player_1)
+        end
     end
-end
-describe Game do 
-    subject(:game) { described_class.new(player_1, player_2) }
-    let(:player_1) { Player.new }
-    let(:player_2) { Player.new }
-    it 'retrieves Player 2' do
-        expect(subject.player_2).to eq(player_2)
+
+    describe '#player_2' do 
+        it 'retrieves Player 2' do
+            expect(subject.player_2).to eq(player_2)
+        end
     end
 end
