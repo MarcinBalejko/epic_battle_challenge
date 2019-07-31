a40c02092075adb8651de2ce4d9831a92524087e
MarcinBalejko <mbbm9@o2.pl>
2019-08-01 00:30:01 +0200

    :white_check_mark: Player 1 can select one of the characters

---
+++ spec/features/selection_spec.rb
@@ -0,0 +1,7 @@
+feature 'Player 1 character select' do
+    scenario 'Player 1 can select one of the characters' do
+        sign_in_and_play
+        click_button('Peter')
+        expect(page).not_to have_content('Error')
+    end
+end
