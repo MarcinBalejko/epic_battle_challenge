1564e47f5f94c9897ae456d614d9128f48b2c7bc
MarcinBalejko <mbbm9@o2.pl>
2019-08-01 01:10:27 +0200

    :sparkle: :white_check_mark: sign_in_and_select

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,4 +1,11 @@
 def sign_in_and_play
     visit('/')
-    click_button 'Fight!'
+    click_button('Fight!')
+end
+
+def sign_in_and_select
+    visit('/')
+    click_button('Fight!')
+    click_button('Peter')
+    click_button('Chicken')
 end
