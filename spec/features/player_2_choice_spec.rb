85539eb244b62bfd57cd2f59c3808e7a22710326
MarcinBalejko <mbbm9@o2.pl>
2019-08-04 00:52:53 +0200

    :recycle: Tests refactored

--- spec/features/player_2_choice_spec.rb
+++ spec/features/player_2_choice_spec.rb
@@ -1,6 +1,6 @@
 feature 'Both players ready to fight' do
     scenario 'Both players selected their characters' do
         sign_in_and_select
-        expect(page).to have_button('Fight!')
+        expect(page).not_to have_content('Error')
     end
 end
