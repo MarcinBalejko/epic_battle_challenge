4786520303180cb4db64dcefeeab5c955993a423
MarcinBalejko <mbbm9@o2.pl>
2019-08-03 18:12:52 +0200

    :white_check_mark: Shows the opponent of the player / Switches turns

--- lib/game.rb
+++ lib/game.rb
@@ -12,6 +12,19 @@ class Game
         @players.last
     end
 
+    def opponent_of(player)
+        players_who_are_not(player).first
+    end
 
+    def switch_turns
+        @current_turn = opponent_of(current_turn)
+    end
+
+    private
+    attr_reader :players
+
+    def players_who_are_not(the_player)
+        players.select { |player| player != the_player }
+    end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -22,5 +22,17 @@ describe Game do
         end
     end
 
-
+    describe '#switch_turns' do
+        it 'switches the turn' do
+            game.switch_turns
+            expect(game.current_turn).to eq player_2
+        end
+    end
+    
+    describe '#opponent_of' do
+        it 'finds the opponent of a player' do
+            expect(game.opponent_of(player_1)).to eq player_2
+            expect(game.opponent_of(player_2)).to eq player_1
+        end
+    end
 end
