0fbffa16038c4966da32693b5c578fc0edc73ffb
MarcinBalejko <mbbm9@o2.pl>
2019-08-07 00:43:17 +0200

    :recycle: Refactored

--- spec/features/main_page_spec.rb
+++ spec/features/main_page_spec.rb
@@ -1,13 +1,13 @@
 feature 'Enter the main page' do
     scenario 'Player can enter the main page' do
         visit('/')
-        expect(page).to have_selector(:link_or_button, 'Fight!')
+        expect(page).to have_selector(:link_or_button, 'FIGHT!')
     end
 end
 feature 'Enter the character selection page' do
     scenario 'Player can proceed to the next page' do
         visit('/')
-        click_button 'Fight!'
+        click_button 'FIGHT!'
         expect(page).to have_content('Select Your Character')
     end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,11 +1,11 @@
 def sign_in_and_play
     visit('/')
-    click_button('Fight!')
+    click_button('FIGHT!')
 end
 
 def sign_in_and_select
     visit('/')
-    click_button('Fight!')
+    click_button('FIGHT!')
     click_button('Peter')
     click_button('Chicken')
 end
