e52f9455985c65f112bf5d0329c147ff5acc63ea
MarcinBalejko <mbbm9@o2.pl>
2019-08-08 01:12:47 +0200

    :recycle: Refactored

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -7,7 +7,9 @@ feature 'Attack the opponent' do
     scenario 'Player 2 can attack Player 1' do
         sign_select_fight
         click_button 'PUNCH'
+        click_button 'Attack complete!'
         click_button 'KICK'
+        click_button 'Attack complete!'
         expect(page).to have_content("PETER'S TURN !!!")
     end
 end
