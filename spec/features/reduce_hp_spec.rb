b0bfeb9a8db21048fae6714b154611df3bd28d63
MarcinBalejko <mbbm9@o2.pl>
2019-08-08 00:58:00 +0200

    :white_check_mark: Player 2 HP is reduced after attack

--- spec/features/reduce_hp_spec.rb
+++ spec/features/reduce_hp_spec.rb
@@ -2,6 +2,7 @@ feature 'Reducing HP' do
     scenario 'Player 2 HP is reduced after attack' do
         sign_select_fight
         click_button 'PUNCH'
-        expect(page).to have_content("90 / CHICKEN")
+        click_button 'Attack complete!'
+        expect(page).to have_content("90 \\ CHICKEN")
     end
 end
