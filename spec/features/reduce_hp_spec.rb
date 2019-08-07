ee6f37cb1649ddee0d7b223065741bcd3d0c9a60
MarcinBalejko <mbbm9@o2.pl>
2019-08-08 01:15:10 +0200

    :white_check_mark: HP reduction with accordance to different attack types

--- spec/features/reduce_hp_spec.rb
+++ spec/features/reduce_hp_spec.rb
@@ -5,4 +5,9 @@ feature 'Reducing HP' do
         click_button 'Attack complete!'
         expect(page).to have_content("90 \\ CHICKEN")
     end
+    scenario 'HP reduction with accordance to different attack types' do
+        sign_select_fight
+        three_round_fight
+        expect(page).to have_content("40 \\ CHICKEN")
+    end
 end
