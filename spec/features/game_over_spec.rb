b1e00193e8b95d37d807027e1be65914855569f7
MarcinBalejko <mbbm9@o2.pl>
2019-08-09 22:57:21 +0200

    :white_check_mark: The winner is revealed at the end

--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -4,7 +4,9 @@ feature 'Game over' do
         defeat_first_player
         expect(page).not_to have_content("PETER'S TURN !!!")
     end
-    scenario 'the winner is revealed' do
-
+    scenario 'the winner can be revealed' do
+        sign_select_fight
+        defeat_first_player
+        expect(page).to have_content("CHICKEN WINS !!!")
     end
 end
