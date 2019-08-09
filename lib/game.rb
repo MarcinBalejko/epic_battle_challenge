864ae9a5d8dbc6e92819eab019f4f7320d18e7f6
MarcinBalejko <mbbm9@o2.pl>
2019-08-09 15:46:40 +0200

    :white_check_mark: game over / loser

--- lib/game.rb
+++ lib/game.rb
@@ -20,7 +20,20 @@ class Game
         @current_turn = opponent_of(current_turn)
     end
 
+    def game_over?
+        losing_players.any?
+    end
     
+    def loser
+        losing_players.first
+    end
+
+    private
+    attr_reader :players
+
+    def losing_players
+        players.select { |player| player.hit_points <= 0 }
+    end
 
     def players_who_are_not(the_player)
         players.select { |player| player != the_player }
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,14 @@
 require 'game'
 describe Game do 
     subject(:game) { described_class.new(player_1, player_2) }
+    subject(:finished_game) { described_class.new(dead_player, player_2) }
     let(:player_1) { Player.new }
     let(:player_2) { Player.new }
+    let(:dead_player) { double :player, hit_points: 0 }
 
     describe '#player_1' do
         it 'retrieves Player 1' do
-            expect(subject.player_1).to eq(player_1)
+            expect(game.player_1).to eq(player_1)
         end
     end
 
@@ -35,4 +37,19 @@ describe Game do
             expect(game.opponent_of(player_2)).to eq player_1
         end
     end
+
+    describe '#game_over?' do
+        it 'returns false if no-one is at 0HP' do
+            expect(game.game_over?).to be false
+        end
+        it 'returns true if at least one player is at 0HP' do
+            expect(finished_game.game_over?).to be true
+        end
+    end
+    describe '#loser' do
+        it 'returns a player on less than 0HP' do
+            expect(finished_game.loser).to eq dead_player
+        end
+    end
+
 end
