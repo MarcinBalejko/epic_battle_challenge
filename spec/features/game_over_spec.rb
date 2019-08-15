4cf37b72f808e630b42d86b255248af59c815612
MarcinBalejko <mbbm9@o2.pl>
2019-08-16 00:19:35 +0200

    :recycle: Refactored

--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -9,5 +9,9 @@ feature 'Game over' do
         defeat_first_player
         expect(page).to have_content("CHICKEN", count: 2)
     end
-  
+    scenario 'loser\'s image changes' do
+        sign_select_fight
+        defeat_first_player
+        expect(page).to have_css("img[src*='peter_beaten.png']")
+    end
 end
