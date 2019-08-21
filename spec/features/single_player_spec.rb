0e5cc4737aa0f8faf3ce7c5b88573bfe6c1d4297
MarcinBalejko <mbbm9@o2.pl>
2019-08-21 02:52:14 +0200

    :white_check_mark: Single player - Computer selects its own character

--- spec/features/single_player_spec.rb
+++ spec/features/single_player_spec.rb
@@ -13,3 +13,13 @@ feature 'Select game mode' do
         page.should have_button('Chicken')
     end
 end
+feature 'Computer randomly selected character' do
+    scenario 'Computer controlled player randomly selects his own character' do
+        visit('/')
+        click_button('FIGHT!')
+        click_button('SINGLE PLAYER')
+        click_button('Peter')
+        visit('/attack_single_p1')
+        expect(page).not_to have_css("img[src*='blank2.png']")
+    end
+end
