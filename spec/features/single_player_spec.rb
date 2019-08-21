4df408b7738cfb01b0c6e4b44f5f554eb22daca1
MarcinBalejko <mbbm9@o2.pl>
2019-08-21 02:53:56 +0200

    :recycle: Refactored

--- spec/features/single_player_spec.rb
+++ spec/features/single_player_spec.rb
@@ -1,13 +1,11 @@
 feature 'Select game mode' do
     scenario 'Player can select single player mode' do
-        visit('/')
-        click_button('FIGHT!')
+        sign_in_and_play
         click_button('SINGLE PLAYER')
         expect(page).to have_content 'Select Your Character'
     end
     scenario 'Player can select player vs player mode' do
-        visit('/')
-        click_button('FIGHT!')
+        sign_in_and_play
         click_button('PLAYER VS PLAYER')
         click_button('Peter')
         page.should have_button('Chicken')
@@ -15,8 +13,7 @@ feature 'Select game mode' do
 end
 feature 'Computer randomly selected character' do
     scenario 'Computer controlled player randomly selects his own character' do
-        visit('/')
-        click_button('FIGHT!')
+        sign_in_and_play
         click_button('SINGLE PLAYER')
         click_button('Peter')
         visit('/attack_single_p1')
